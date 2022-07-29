312d17393d137b25deba632f188fd87ff71a6f71
bnomei <b@bnomei.com>
2022-07-29 10:16:11 +0100

    :wrench: tracking success
    Signed-off-by: bnomei <b@bnomei.com>

--- src/CacheBenchmark.php
+++ src/CacheBenchmark.php
@@ -9,6 +9,8 @@ use Bnomei\Driver\Redis;
 
 class CacheBenchmark
 {
+    public const ITERATIONS = 10000;
+
     public static function benchmark(Driver $driver, array $options = []): array
     {
         $results = [];
@@ -17,13 +19,17 @@ class CacheBenchmark
         $driver->flush();
         foreach(['testSet', 'testGet', 'testTransaction'] as $test) {
             $time = -microtime(true);
-            self::{$test}($driver);
+            $success = self::{$test}($driver);
             $time = round(($time + microtime(true)) * 1000);
-            $results[$test] = $time;
+            $results[$test] = [
+                'duration' => $time,
+                'iterations' => self::ITERATIONS,
+                'success' => $success,
+            ];
             $duration += $time;
         }
-        $results['duration'] = $duration;
-        $results['options'] = $options;
+        $results['total_duration'] = $duration;
+        $results['driver_options'] = $options;
 
         return $results;
     }
@@ -41,58 +47,65 @@ class CacheBenchmark
         return $results;
     }
 
-    public static function apcu(array $options = []): array
+    public static function apcu(array $options = [], string $label = null): array
     {
-        return ['apcu' => self::benchmark(new Apcu(), $options)];
+        return [ ($label ?? 'apcu') => self::benchmark(new Apcu(), $options)];
     }
 
-    public static function memcached(array $options = []): array
+    public static function memcached(array $options = [], string $label = null): array
     {
-        return ['memcached' => self::benchmark(new Memcached($options), $options)];
+        return [ ($label ?? 'memcached') => self::benchmark(new Memcached($options), $options)];
     }
 
-    public static function redis(array $options = []): array
+    public static function redis(array $options = [], string $label = null): array
     {
-        return ['redis' => self::benchmark(new Redis($options), $options)];
+        return [ ($label ?? 'redis') => self::benchmark(new Redis($options), $options)];
     }
 
-    public static function testSet(Driver $driver)
+    public static function testSet(Driver $driver): bool
     {
-        for($i = 0; $i < 50000; $i++) {
-            $driver->set('key-'. $i, __FILE__ . $i);
+        for($i = 0; $i < self::ITERATIONS; $i++) {
+            $success = $driver->set('key-'. $i, __FILE__ . $i);
+            if (! $success) {
+                return false;
+            }
         }
+        return true;
     }
 
     public static function testGet(Driver $driver): bool
     {
-        $success = false;
-        for($i = 0; $i < 50000; $i++) {
+        for($i = 0; $i < self::ITERATIONS; $i++) {
             $value = $driver->get('key-'. $i) !== __FILE__ . $i;
             if (!$value) {
-                $success = false;
+                return false;
             }
         }
-        return $success;
+        return true;
     }
 
-    public static function testTransaction(Driver $driver)
+    public static function testTransaction(Driver $driver): bool
     {
         $driver->beginTransaction();
-        $success = false;
-        for($i = 1; $i <= 100000; $i++) {
+        for($i = 1; $i <= self::ITERATIONS * 2; $i++) {
             if($i % 2 === 0) {
-                // retrieve 1 on 2, retrieve 3 on 4
+                // retrieve 1 on 2, retrieve 3 on 4, ...
                 $value = $driver->get('tkey-'. ($i - 1)) !== __FILE__ . ($i - 1);
                 if (!$value) {
-                    $success = false;
+                    $driver->endTransaction();
+                    return false;
                 }
             } else {
                 // set on 1, set on 3, ...
-                $driver->set('tkey-'. $i,  __FILE__ . $i);
+                $success = $driver->set('tkey-'. $i,  __FILE__ . $i);
+                if (! $success) {
+                    $driver->endTransaction();
+                    return false;
+                }
             }
 
         }
         $driver->endTransaction();
-        return $success;
+        return true;
     }
 }
